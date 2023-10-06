83f1f64ea2a146d652f51248eb7204626c97a0bd
Lintangeaa <lindprak@gmail.com>
2023-10-06 10:08:46 +0700

    benerin

--- app/components/organism/HomeCardList.js
+++ app/components/organism/HomeCardList.js
@@ -6,26 +6,33 @@ const HomeCardList = () => {
   return (
     <div className="flex justify-between w-full space-x-4">
       <Card title={'SERVICES'}>
-        <div className="flex justify-between p-5 space-x-4 border rounded-xl border-six">
+        <div className="flex justify-between w-full max-w-[100px] p-5 space-x-4 border lg:max-w-[200px] rounded-xl border-six">
           <Image
             src={'nodejs.svg'}
             width={80}
             height={50}
             alt="Lintang Dandung Prakoso"
+            className="max-w-[30px] lg:max-w-full"
           />
           <Image
             src={'/python.png'}
             width={50}
             height={50}
             alt="Lintang Dandung Prakoso"
+            className="max-w-[30px] lg:max-w-full"
           />
         </div>
       </Card>
       <Card title={'PROJECT'}>
-        <div className="flex justify-between p-5 space-x-4 border rounded-full border-six"></div>
+        <Image
+          src={'/laptop.png'}
+          width={200}
+          height={50}
+          alt="Lintang Dandung Prakoso"
+        />
       </Card>
       <Card title={'ABOUT'}>
-        <div className="flex justify-between p-5 space-x-4 border rounded-full border-six"></div>
+        <p className="font-semibold text-8xl text-primary">A</p>
       </Card>
     </div>
   );
